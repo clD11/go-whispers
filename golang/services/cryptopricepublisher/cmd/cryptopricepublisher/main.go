87db7353aed6d9e3ad504ef87bd4e56fe45e05fe
clD11 <23483715+clD11@users.noreply.github.com>
2021-02-12 23:33:34 +0000

    WIP - started kraken client

--- golang/services/cryptopricepublisher/cmd/cryptopricepublisher/main.go
+++ golang/services/cryptopricepublisher/cmd/cryptopricepublisher/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/clD11/go-whispers/golang/services/cryptopricepublisher/internal"
-)
-
 func main() {
-	internal.GetTickerInformation()
-	fmt.Println("End Client")
+
 }
--- golang/services/twitterstreamproducer/cmd/twitterstreamproducer/main.go
+++ golang/services/twitterstreamproducer/cmd/twitterstreamproducer/main.go
@@ -2,30 +2,9 @@ package main
 
 import (
 	"fmt"
-	. "github.com/clD11/go-whispers/services/twitterstreamproducer/internal"
-	"time"
 )
 
 func main() {
 	fmt.Println("Starting Twitter Stream Producer")
-
-	twitterStreamWrapperConfig := TwitterStreamWrapperConfig{
-		APIKey:            "test",
-		APISecretKey:      "test",
-		AccessToken:       "test",
-		AccessTokenSecret: "test",
-	}
-
-	twitterStreamWrapperFilter := TwitterStreamWrapperFilter{
-		Track: []string{"ETH,BTC"},
-	}
-
-	twitterStreamWrapper := NewTwitterStreamWrapper(twitterStreamWrapperConfig, twitterStreamWrapperFilter)
-	twitterStreamWrapper.Start()
-
-	time.Sleep(30 * time.Second)
-
 	fmt.Println("Stopping Twitter Stream Producer")
-
-	twitterStreamWrapper.Stop()
 }

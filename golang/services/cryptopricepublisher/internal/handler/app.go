e0418aaebff05ea36fdfd33aad66e841cf2dcdb0
clD11 <23483715+clD11@users.noreply.github.com>
2021-02-14 21:04:12 +0000

    WIP - krakend config

--- golang/services/cryptopricepublisher/internal/handler/app.go
+++ golang/services/cryptopricepublisher/internal/handler/app.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 )
 
 func GetLanding(w http.ResponseWriter, r *http.Request) {
+	log.Println("Splash!")
+	w.WriteHeader(200)
 	w.Write([]byte("Cryptocurrency Price Publisher - Using Kraken API"))
 }

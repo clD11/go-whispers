2605af81a0c5cc3549aef99a26b9e2372071b1a2
clD11 <23483715+clD11@users.noreply.github.com>
2021-02-17 21:23:27 +0000

    WIP - Creating gource to gif

---
+++ .github/scripts/convert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gource -1280x720 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
+ffmpeg -ss 30 -t 3 -i gource.mp4 -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 whispers.gif
--- .github/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-printf 'Running Test Script'
-
-apt-get update
-apt-get upgrade
-
-ffmpeg -version
